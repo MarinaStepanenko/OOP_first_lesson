1a7e11f4b3a6704cb1d429c8cf8f95357cbcbb17
stepanenko <m4liwka007@yandex.ru>
2025-12-21 17:20:41 +0300

    второе задание с тестами

--- src/category.py
+++ src/category.py
@@ -4,14 +4,28 @@ from src.product import Product
 class Category:
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product]
     category_count = 0
     product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
-        Category.product_count += sum([product.quantity for product in products])
+        Category.product_count += len(products)
+
+    def add_product(self, product: Product):
+        if product not in self.__products:
+            self.__products.append(product)
+            Category.product_count += 1
+
+    @property
+    def products(self):
+        products_list = []
+        for product in self.__products:
+            products_list.append(
+                f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+            )
+        return "\n".join(products_list)
