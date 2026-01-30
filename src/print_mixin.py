6f4008c77920cb094faa6efa3f2358cea59f961c
stepanenko <m4liwka007@yandex.ru>
2026-01-30 11:29:53 +0300

    создание абстрактного базового класса, создание класса миксин, тесты

--- src/print_mixin.py
+++ src/print_mixin.py
@@ -6,13 +6,3 @@ class PrintMixin:
 
     def __repr__(self):
         return f"{self.__class__.__name__}, {self.name}, {self.description}, {self.price}, {self.quantity}"
-
-
-def check_mro():
-    from src.product import Product  # Локальный импорт внутри функции
-
-    print(Product.__mro__)
-
-
-if __name__ == "__main__":
-    check_mro()
